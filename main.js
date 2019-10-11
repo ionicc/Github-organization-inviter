809c7b4fe3557dcc56b16a81b01d6644b5627c9d
Sagar Vakkala <sagar@codezoned.com>
2019-10-11 09:21:28 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -2,33 +2,41 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 const Octokit = require('@octokit/rest');
+var bodyParser = require('body-parser');
 
 // var ipv4 = constants.ipv4;
 // var fport = constants.port;
 
-var constants = require("./lib/constant.js");
+const ORGANIZATION = "";
+const TOKEN = ""
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 var client = Octokit({
-    auth: constants.octoToken
+    auth: TOKEN
+});
+
+app.post('/input_data',(req,res) => {
+    if(req.body.handle == "") res.redirect('back'); 
+    var invStatus = client.orgs.createInvitation({
+        org: ORGANIZATION,
+        email: req.body.email
+    });
+    console.log("Invitation sent to" + req.body.handle + "At : " + req.body.email);
+    res.send("Invitation sent to" + req.body.handle + "At : " + req.body.email);
 });
 
 /*
-function user_exists(email) {
-    client.
-}
-
-client.orgs.createInvitation({
-    org: constants.orgName,
-    email: ""
-});
-console.log(client);
+var user = client.users.getByUsername({
+    username:"ionicc"
+}).then(console.log(user));
 */
+
 http.listen(3000,function(){
     console.log('listening on : 3000');
 });
-
